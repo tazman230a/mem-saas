e43cb25c13b6b5fc8c6d7269c743a5fc086c5c1a
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-08-22 22:07:46 -0700

    implemented auth, billing, database

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>
-        Let&apos;s get started
-      </Button>
+    <main>
+      <h1>Popular Companions</h1>
+       <section className='home-section'>
+         <CompanionCard 
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+         />
+         <CompanionCard 
+            id="456"
+            name="The Growth Expert"
+            topic="Scaling Your Business Successfully"
+            subject="Business"
+            duration={20}
+            color="#e5d0ff"
+         />
+         <CompanionCard 
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="English Literature"
+            subject="Language"
+            duration={30}
+            color="#bde7ff"
+         />
+       </section>
 
-    </div>
+       <section className='home-section'>
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+       </section>
+    </main>
   )
 }
 
