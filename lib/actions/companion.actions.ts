87e14e65f3fcac7283109599f249f42f2f577de0
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-08-23 19:59:02 -0700

    Implement library, search,  filter

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -2,7 +2,7 @@
 
 import {auth} from "@clerk/nextjs/server";
 import {createSupabaseClient} from "@/lib/supabase";
-import { revalidatePath } from "next/cache";
+//import { revalidatePath } from "next/cache";
 
 
 export const createCompanion = async (formData: CreateCompanion) => {
@@ -17,4 +17,27 @@ export const createCompanion = async (formData: CreateCompanion) => {
         if (error || !data) throw new Error(error?.message || 'Failed to create companion')
 
         return data[0];
+}
+
+export const getAllCompanions = async ({ limit = 10, page =1, subject, topic }: GetAllCompanions) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from("companions").select();
+
+  if(subject && topic) {
+    query = query.ilike( 'subject', `%${subject}%`)
+        .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+  } else if(subject) {
+    query = query.ilike('subject', `%${subject}%`)
+  } else if(topic) {
+    query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+  }
+
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const {data: companions, error } = await query;
+
+  if(error) throw new Error(error.message);
+
+  return companions;
 }
